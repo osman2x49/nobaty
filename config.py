d2608f40363561b26c49ee6009174bda83276d6a
osman2x49 <samahhisham64@gmail.com>
2025-09-27 22:52:38 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+# إعدادات البوت - احتفظ بهذا الملف سراً ولا ترفعه على GitHub
+BOT_TOKEN = "8238640391:AAEnXk0NNHmEKSWGLpQ4ySy9PdvWH7xtF7k"
+ADMIN_USERNAME = "a"
+ADMIN_PASSWORD = "199"
+
+# في bot.py استبدل الإعدادات بـ:
+from config import BOT_TOKEN, ADMIN_USERNAME, ADMIN_PASSWORD
